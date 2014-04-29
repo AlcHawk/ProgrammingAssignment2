688b7aca8c1a118869bc56d579ef89c5acc6b771
AlcHawk <AlcHawk@Wei-Chihs-MacBook-Pro.local>
2014-04-30 01:25:45 +0800

    Second commit for cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+## Creating a cache of matrix for the other function use 
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -10,7 +12,7 @@ makeCacheMatrix <- function(x = matrix()) {
         m <<- NULL
     }
     get <- function() x
-    setInverse <- function(mean) m <<- mean
+    setInverse <- function(Inverse) m <<- Inverse
     getInverse <- function() m
     list(set = set, get = get,
          setInverse = setInverse,
@@ -22,4 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getInverse()           #query the x Inverse's cache         
+    if(!is.null(m)) {           #if there is a cache
+        message("getting cached data") 
+        return(m)                #just return the cache, no computation needed
+    }
+
+    Iv <- x$get()             #if there's no cache
+    det <- (Iv[1,1]*Iv[2,2] - Iv[1,2]*Iv[2,1])
+    m <- 1/(det)*matrix(c(Iv[2,2], Iv[2,1], Iv[1,2], Iv[1,1]), 2)        #we actually compute them here
+    x$setInverse(m)                #save the result back to x's cache
+    m                           #return the result
 }
